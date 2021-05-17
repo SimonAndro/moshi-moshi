48f77b6e43e836471935e5eead6fa0022914daa0
ogrigriii <57384448+Coachsteveee@users.noreply.github.com>
2021-05-17 22:14:54 +0800

    changed start to begin

--- app/views/home/index.html.php
+++ app/views/home/index.html.php
@@ -332,7 +332,7 @@
                             <button id="select-close" type="button" class="close" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
-                            <h4 class="modal-title">Start Chat</h4>
+                            <h4 class="modal-title">Begin Chat</h4>
 
                             <hr>
 
